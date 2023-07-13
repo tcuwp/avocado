b0c1c8c69d5e128b516e780095b50f5e8550f0dd
Matt Jankowski <matt@jankowski.online>
2023-07-13 14:22:01 -0400

    Remove empty test

--- spec/avocado_spec.rb
+++ spec/avocado_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Avocado do
   it "has a version number" do
     expect(Avocado::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
