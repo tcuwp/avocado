c08ba1c8315bdb4566463c239346e03387b0083b
Matt Jankowski <matt@jankowski.online>
2023-07-13 14:14:07 -0400

    Initial `bundle gem` generation

---
+++ spec/avocado_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Avocado do
+  it "has a version number" do
+    expect(Avocado::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
