5063b290b06dd30c36d86d0b790346ffd0425183
Matt Jankowski <matt@jankowski.online>
2023-07-14 17:03:15 -0400

    Add format requirement for passwords

--- lib/avocado/user_password_concern.rb
+++ lib/avocado/user_password_concern.rb
@@ -6,12 +6,13 @@ module Avocado
   module UserPasswordConcern
     extend ActiveSupport::Concern
 
+    REQUIRED_FORMAT = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*\z/x
     REQUIRED_LENGTH = 8
 
     included do
       has_secure_password
 
-      validates :password, length: {minimum: REQUIRED_LENGTH}
+      validates :password, format: {with: REQUIRED_FORMAT}, length: {minimum: REQUIRED_LENGTH}
     end
   end
 end
