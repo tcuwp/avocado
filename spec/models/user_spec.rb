4f539bde031e33da53dc1a410984920df246ff3b
Matt Jankowski <matt@jankowski.online>
2023-07-14 14:40:45 -0400

    Add Authentication block to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe User do
-  it "authenticates with email and password" do
-    user_params = {email: "test@host.example", password: "test"}
-    user = described_class.new(user_params)
-    user.save
+  describe "Authentication" do
+    it "authenticates with email and password" do
+      user_params = {email: "test@host.example", password: "test"}
+      user = described_class.new(user_params)
+      user.save
 
-    expect(described_class.authenticate_by(user_params)).to eq(user)
+      expect(described_class.authenticate_by(user_params)).to eq(user)
+    end
   end
 end
