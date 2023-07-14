8c6b44b2d97d9badebc25f7a1f6534439deb96aa
Matt Jankowski <matt@jankowski.online>
2023-07-13 22:42:40 -0400

    Require the user concern from main lib

--- lib/avocado.rb
+++ lib/avocado.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "avocado/version"
+require_relative "avocado/user_concern"
 
 module Avocado
   class Error < StandardError; end
-  # Your code goes here...
 end
