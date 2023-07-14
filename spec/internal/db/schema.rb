9887284159bcd77224954a5a0e03da16e146c063
Matt Jankowski <matt@jankowski.online>
2023-07-13 22:41:35 -0400

    Generate combustion app and add user model

---
+++ spec/internal/db/schema.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+ActiveRecord::Schema.define do
+  create_table(:users, force: true) do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+end
