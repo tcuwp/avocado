f285dd68784529b43512614283c8c56b39499358
Matt Jankowski <matt@jankowski.online>
2023-07-14 16:12:54 -0400

    Validate email format on user

--- lib/avocado/user_concern.rb
+++ lib/avocado/user_concern.rb
@@ -9,7 +9,7 @@ module Avocado
     included do
       has_secure_password
 
-      validates :email, presence: true, uniqueness: true
+      validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
     end
   end
 end
