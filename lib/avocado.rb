83a64fc807ca0fbe52578c78912913c1a44f755e
Matt Jankowski <matt@jankowski.online>
2023-07-15 09:16:04 -0400

    Use autoload on concerns

--- lib/avocado.rb
+++ lib/avocado.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "avocado/version"
-require_relative "avocado/user_concern"
-require_relative "avocado/user_email_concern"
-require_relative "avocado/user_password_concern"
 
 module Avocado
   class Error < StandardError; end
+
+  autoload :UserConcern, "avocado/user_concern"
+  autoload :UserEmailConcern, "avocado/user_email_concern"
+  autoload :UserPasswordConcern, "avocado/user_password_concern"
 end
