b1afb00b53fc524129029a590ceda1a9b4ce16fb
Matt Jankowski <matt@jankowski.online>
2023-07-16 08:00:43 -0400

    Extract password recovery module

--- lib/avocado/user_password.rb
+++ lib/avocado/user_password.rb
@@ -8,22 +8,11 @@ module Avocado
 
     REQUIRED_FORMAT = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*\z/x
     REQUIRED_LENGTH = 8
-    TOKEN_EXPIRATION = 64.minutes
 
     included do
       has_secure_password
 
       validates :password, format: {with: REQUIRED_FORMAT}, length: {minimum: REQUIRED_LENGTH}, allow_nil: true
-
-      generates_token_for :password_recovery, expires_in: TOKEN_EXPIRATION do
-        password_digest_salt
-      end
-    end
-
-    private
-
-    def password_digest_salt
-      BCrypt::Password.new(password_digest).salt[-10..]
     end
   end
 end
