13d606a501798f26b88ea57f9b5cf11b5bc7dfbd
Matt Jankowski <matt@jankowski.online>
2023-07-18 15:18:50 -0400

    Give spec/internal app a root route

--- spec/internal/config/routes.rb
+++ spec/internal/config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: "languages#index"
   # Add your own routes here, or remove this file if you don't have need for it.
 end
