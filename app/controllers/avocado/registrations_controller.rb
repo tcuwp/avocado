44b9ee998ea31fa1dc14939abbe63ad004031e40
Matt Jankowski <matt@jankowski.online>
2023-07-18 17:31:48 -0400

    Add authentication module

--- app/controllers/avocado/registrations_controller.rb
+++ app/controllers/avocado/registrations_controller.rb
@@ -4,6 +4,8 @@ module Avocado
   class RegistrationsController < BaseController
     PERMITTED_PARAMS = [:email, :password, :password_confirmation]
 
+    skip_before_action :authenticate
+
     def new
       @user = ::User.new
     end
@@ -12,6 +14,8 @@ module Avocado
       @user = ::User.new(user_params)
 
       if @user.save
+        sign_in(@user)
+
         send_email_verification
         redirect_to root_path, notice: "Registration successful"
       else
---
+++ lib/avocado/authentication.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Avocado
+  module Authentication
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :set_current_request_details
+      before_action :authenticate
+
+      helper_method :current_user
+      helper_method :signed_in?
+      helper_method :current_session
+    end
+
+    def current_user
+      Current.user
+    end
+
+    def signed_in?
+      current_user.present?
+    end
+
+    def current_session
+      Current.session
+    end
+
+    private
+
+    def authenticate
+      if session_from_token
+        Current.session = session_from_token
+      else
+        redirect_to new_session_path
+      end
+    end
+
+    def sign_in(user)
+      ::Session.create!(user: user).tap do |session|
+        cookies.signed.permanent[:session_token] = {value: session.id, httponly: true}
+      end
+    end
+
+    def session_from_token
+      ::Session.find_by_id(cookies.signed[:session_token])
+    end
+
+    def set_current_request_details
+      Current.user_agent = request.user_agent
+      Current.ip_address = request.ip
+    end
+  end
+end
