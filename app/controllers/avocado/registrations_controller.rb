64ec24b9ed5e647c6b8dc4f6798487bd386be443
Matt Jankowski <matt@jankowski.online>
2023-07-18 15:29:57 -0400

    Add registrations#create action

--- app/controllers/avocado/registrations_controller.rb
+++ app/controllers/avocado/registrations_controller.rb
@@ -2,8 +2,36 @@
 
 module Avocado
   class RegistrationsController < BaseController
+    PERMITTED_PARAMS = [:email, :password, :password_confirmation]
+
     def new
       @user = ::User.new
+    end
+
+    def create
+      @user = ::User.new(user_params)
+
+      if @user.save
+        send_email_verification
+        redirect_to root_path, notice: "Registration successful"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params
+        .require(:user)
+        .permit(PERMITTED_PARAMS)
+    end
+
+    def send_email_verification
+      Avocado::Mailer
+        .with(user: @user)
+        .email_verification
+        .deliver_later
     end
   end
 end
