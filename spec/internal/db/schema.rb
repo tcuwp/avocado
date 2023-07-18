27d40dec169e0405177c3a82d6d2f36958d8582b
Matt Jankowski <matt@jankowski.online>
2023-07-18 17:29:19 -0400

    Add sessions table to internal spec app schema

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 ActiveRecord::Schema.define do
-  create_table(:users, force: true) do |t|
+  create_table "sessions", force: true do |t|
+    t.bigint "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: true do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
