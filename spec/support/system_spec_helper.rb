a6e16c45efa4438669874500fe6fcc268bced79d
Matt Jankowski <matt@jankowski.online>
2023-07-19 12:55:34 -0400

    Move sign_in helper to support

---
+++ spec/support/system_spec_helper.rb
@@ -0,0 +1,8 @@
+module SystemSpecHelper
+  def sign_in(user)
+    visit new_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
