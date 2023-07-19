9a9740193f927f36d0511b227537168550b78005
Matt Jankowski <matt@jankowski.online>
2023-07-19 11:19:00 -0400

    Formatting fix in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User do
     describe ".verified" do
       it "returns only verified users" do
         verified_user = create(:user, verified: true)
-        un_verified_user = create(:user, verified: false)
+        _unverified_user = create(:user, verified: false)
 
         expect(described_class.verified).to eq [verified_user]
       end
