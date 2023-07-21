9043fc4608370a92fe82cf83a58559719123c001
Matt Jankowski <matt@jankowski.online>
2023-07-21 11:28:40 -0400

    Once over on callback naming

--- lib/avocado/event.rb
+++ lib/avocado/event.rb
@@ -16,10 +16,14 @@ module Avocado
 
       validates :action, inclusion: VALID_ACTIONS
 
-      before_create do
-        self.user_agent = Current.user_agent
-        self.ip_address = Current.ip_address
-      end
+      before_create :capture_request_details
+    end
+
+    private
+
+    def capture_request_details
+      self.user_agent = Current.user_agent
+      self.ip_address = Current.ip_address
     end
   end
 end
--- lib/avocado/session.rb
+++ lib/avocado/session.rb
@@ -13,20 +13,24 @@ module Avocado
 
       after_destroy :record_activity_destroy
 
-      before_create :record_request_details
+      before_create :capture_request_details
     end
 
     private
 
     def record_activity_create
-      user.events.create! action: "session:create"
+      create_user_event "session:create"
     end
 
     def record_activity_destroy
-      user.events.create! action: "session:destroy"
+      create_user_event "session:destroy"
     end
 
-    def record_request_details
+    def create_user_event(action)
+      user.events.create! action: action
+    end
+
+    def capture_request_details
       self.user_agent = Current.user_agent
       self.ip_address = Current.ip_address
     end
