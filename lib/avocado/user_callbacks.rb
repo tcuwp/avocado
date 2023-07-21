a13a8e48e062a88e9f7b7e632bd1a823ce1fccc3
Matt Jankowski <matt@jankowski.online>
2023-07-21 12:29:37 -0400

    Group password change callbacks in user callbacks

--- lib/avocado/user_callbacks.rb
+++ lib/avocado/user_callbacks.rb
@@ -5,10 +5,13 @@ module Avocado
     extend ActiveSupport::Concern
 
     included do
-      after_update :destroy_non_current_sessions, if: :password_digest_previously_changed?
+      with_options if: :password_digest_previously_changed? do
+        after_update :destroy_non_current_sessions
+        after_update :record_activity_password_update
+      end
+
       after_update :record_activity_email_update, if: :email_previously_changed?
       after_update :record_activity_email_verified, if: %i[verified_previously_changed? verified?]
-      after_update :record_activity_password_update, if: :password_digest_previously_changed?
 
       before_validation :remove_email_verification, if: :email_changed?, on: :update
     end
