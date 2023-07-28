f2ef3e9efeaa047a0659c260b1d7a09288d3de40
Matt Jankowski <matt@jankowski.online>
2023-07-28 11:58:20 -0400

    Avoid gem build warning about symlinks

--- lib/avocado/engine.rb
+++ lib/avocado/engine.rb
@@ -7,7 +7,7 @@ module Avocado
   class Engine < Rails::Engine
     initializer :avocado_routing do
       ActiveSupport.on_load(:action_dispatch_request) do
-        ActionDispatch::Routing::Mapper.define_method(:🥑) { :🥑 }
+        ActionDispatch::Routing::Mapper.define_method(:🥑) { :avocado }
       end
     end
   end
